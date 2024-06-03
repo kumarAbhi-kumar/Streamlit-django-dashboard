90ce71073353665e9885e6b9b2c4036a238c919a
Kumar Abhishek <fabiamkumar@gmail.com>
2024-06-03 13:44:31 +0530

    Completed the Project

--- README.md
+++ README.md
@@ -1,18 +1,21 @@
 # Analytics Dashboard with Django & Streamlit
 
 1. Create a virtual environment
-    python -m venv env
+    <br>
+    `python -m venv env`
 
-2. Activate the Virtual Environment
-    ./env/Scripts/activate
+2. Activate the Virtual Environment <br>
+    `./env/Scripts/activate`
 
 3. Install all the dependencies
-   pip install -r requirements.txt
+    <br>
+   `pip install -r requirements.txt`
 
 4. Create 2 different terminal
-    a. for the backend.
-        cd backend
-        python manage.py runserver
+    <br>
+    a. for the backend.<br>
+        `cd backend`<br>
+        `python manage.py runserver`
     
-    b. execute the app.py file (Streamlit file)
-        streamlit run app.py
+    b. execute the app.py file (Streamlit file)<br>
+        `streamlit run app.py`
--- app.py
+++ app.py
@@ -1,6 +1,31 @@
 import streamlit as st 
 import matplotlib.pyplot as plt
 import numpy as np
+import altair as alt 
+import pandas as pd
+import requests
+
+# Endpoints
+swapi_endpoint = 'https://swapi.dev/api/people/1/'
+api_url = 'http://127.0.0.1:8000/api/customers/'
+
+# Functions
+
+def fetch_data(endpoint):
+    response = requests.get(endpoint)
+    data = response.json()
+    return data
+
+def send_data(name, gender, age, favorite_number):
+    gender_value = "0" if gender == "Female" else "1"
+    data = {
+        "name": name,
+        "gender": gender_value,
+        "age": age,
+        "favorite_number": favorite_number
+    }
+    response = requests.post(api_url, json=data)
+    return response
 
 st.title("Analytics Dashboard")
 st.write("v.0.0.1")
@@ -40,4 +65,38 @@ if st.button('increment'):
 
 st.write(f"Counter Value: {st.session_state.counter}")
 
+# data from SWAPI API
+swapi_data = fetch_data(swapi_endpoint) # hold the json data returned by the endpoint
 
+st.write('Data From the Swapi API')
+st.json(swapi_data)
+
+# Fetch Data from our Custom API
+data = fetch_data(api_url)
+
+if data:
+    # Creating a tabular view for the file
+    df = pd.DataFrame(data)
+    st.dataframe(df)
+
+    # Adding the scatterplot.
+    scatter_chart = alt.Chart(df).mark_circle().encode(
+        x = 'age',
+        y = 'favorite_number'
+    )
+
+    st.altair_chart(scatter_chart, use_container_width=True)
+
+# Form to collect Customer Data
+name = st.text_input("Your Name")
+gender = st.radio("Select your Gender", {"Male", "Female"})
+age = st.slider("Select your Age", 1, 100, 18)
+favorite_number = st.number_input("Enter your Favorite Number", step = 1)
+
+if st.button("Submit"):
+    response = send_data(name, gender, age, favorite_number)
+    if response.status_code == 201:
+        st.success("New Customer Data created")
+        st.rerun()
+    else:
+        st.error("Something went wrong..!")
